a5e61313417da38078165ed3e723b2052ae0b6a7
johandavidmercado2712-glitch <johandavidmercado2712@gmail.com>
2026-06-04 23:22:10 +0200

    modificacion de variables de entornos y bases de datos

--- backend/main.py
+++ backend/main.py
@@ -22,7 +22,8 @@ origins =[
     "http://localhost:8000",
     "http://localhost:5000",
     "http://localhost:5500",
-    "http://127.0.0.1:5500"
+    "http://127.0.0.1:5500",
+    "http://127.0.0.1:5501"
     
 ]
 
